7f755942084ae1ad20d5a032869fb7d1a1914e0f
loopy-lim <loopy@jnu.ac.kr>
2024-06-26 22:17:52 +0900

    fix: fix end zero to product chap4

--- rust/zero-to-production-in-rust/zero2prod/src/configuration.rs
+++ rust/zero-to-production-in-rust/zero2prod/src/configuration.rs
@@ -10,19 +10,9 @@ pub struct Settings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
-    pub host: String,
     pub port: u16,
+    pub host: String,
     pub database_name: String,
-}
-
-pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
-        ))
-        .build()?;
-    settings.try_deserialize::<Settings>()
 }
 
 impl DatabaseSettings {
@@ -36,7 +26,8 @@ impl DatabaseSettings {
             self.database_name
         ))
     }
-    pub fn connectino_string_without_db(&self) -> Secret<String> {
+
+    pub fn connection_string_without_db(&self) -> Secret<String> {
         Secret::new(format!(
             "postgres://{}:{}@{}:{}",
             self.username,
@@ -45,4 +36,14 @@ impl DatabaseSettings {
             self.port
         ))
     }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+    settings.try_deserialize::<Settings>()
 }
